5708373d907e1abe8e5653471b31b3b9afa2faaf
manikanta639 <manikantanagubandi9@gmail.com>
2025-10-18 17:04:53 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -3,40 +3,54 @@ const urlsToCache = [
   "/",
   "/index.html",
   "/manifest.json",
+  "/styles.css",
+  "/script.js",
   "/icon-192x192.png",
-  "/icon-512x512.png"
-  // Add other assets such as CSS, JS, images as needed
+  "/icon-512x512.png",
+  "/cart.html",
+  "/wishlist.html",
+  "/menu.html",
+  "/products.html",
+  // Add any additional CSS, JS, image files your app requires
 ];
 
 self.addEventListener("install", (event) => {
   console.log("Service Worker installed");
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(urlsToCache);
-    }).then(() => self.skipWaiting())
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(urlsToCache))
+      .then(() => self.skipWaiting()) // Activate the new SW immediately
   );
 });
 
 self.addEventListener("activate", (event) => {
   console.log("Service Worker activated");
   event.waitUntil(
-    caches.keys().then((cacheNames) =>
-      Promise.all(
-        cacheNames.map((cacheName) => {
-          if (cacheName !== CACHE_NAME) {
+    caches.keys()
+      .then((cacheNames) => Promise.all(
+        cacheNames
+          .filter((cacheName) => cacheName !== CACHE_NAME)
+          .map((cacheName) => {
             console.log("Deleting old cache:", cacheName);
             return caches.delete(cacheName);
-          }
-        })
-      )
-    ).then(() => self.clients.claim())
+          })
+      ))
+      .then(() => self.clients.claim()) // Control all clients immediately
   );
 });
 
 self.addEventListener("fetch", (event) => {
   event.respondWith(
     caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
+      if (response) {
+        return response; // Cache hit
+      }
+      return fetch(event.request).catch(() => {
+        // TODO: Return custom offline page for navigation requests
+        if (event.request.mode === "navigate") {
+          return caches.match("/offline.html");
+        }
+      });
     })
   );
 });
